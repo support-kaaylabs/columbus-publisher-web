090c413d7fde5804929acb03f8ef63009120f671
KrishnaPrabu-KL <krishna.prabu@kaaylabs.com>
2023-04-25 21:18:51 +0530

    my profile

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,89 +1,111 @@
 import React, { type FC, useEffect, useState } from 'react';
 import './content.scss';
-import { Col, Row } from 'antd';
+import { Col, Row, Spin } from 'antd';
 import { FilterOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import { get } from 'lodash';
 import { getProduct } from '../../shared/urlHelper';
+import { errorNotification } from '../../shared/globalVariables';
 import Eye from './Images/eyeImg.svg';
 import Hand from './Images/nounClickImg.svg';
 import Arrow from './Images/nounCursorImg.svg';
 
 const ProductList: FC = () => {
-
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(false);
   const userId = localStorage.getItem('User_Uid');
 
-  useEffect(() => {   
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    setLoading(true);
     getProduct({ id: userId })
       .then((res: any) => {
-        if(res.success){
+        if (res.success) {
           setTodos(get(res, 'data', {}));
-        }        
+          setLoading(false);
+        }
+      })
+      .catch((err) => {
+        errorNotification(err);
+        setLoading(false);
       });
-  }, []);
+  };
 
   return (
     <div className="head">
-      <div className="filter-head">
-        <div className="filter">
-          <FilterOutlined />
+      {loading && (
+        <div className="loader">
+          <Spin tip="Loading" size="large">
+            <div className="content" />
+          </Spin>
         </div>
-      </div>
-      <Row className="content-row">
-        {todos &&
-          todos.map((item: any, index) => (
-            <Col key={index} sm={24} md={12} lg={8}>
-              <div className="content-head">
-                <div className="content-logo">
-                  <img src={item.Image} alt={item.Brand} />
-                </div>
-                <div className="content-main">
-                  <div className="content-para-button">
-                    <div className="impression">
-                      <p>IMPRESSION</p>
-                      <button type="button">
-                        <span>
-                          <img src={Eye} alt="Eye" />
-                          <p>{item.Price}</p>
-                        </span>
-                      </button>
+      )}
+      {!loading && (
+        <>
+          <div className="filter-head">
+            <div className="filter">
+              <FilterOutlined />
+            </div>
+          </div>
+          <Row className="content-row">
+            {todos &&
+              todos.map((item: any, index: number) => (
+                <Col key={index} sm={24} md={12} lg={8}>
+                  <div className="content-head">
+                    <div className="content-logo">
+                      <img src={item.Image} alt={item.Brand} />
                     </div>
-                    <div className="clicks">
-                      <p>CLICKS</p>
-                      <button type="button">
-                        <span>
-                          <img src={Hand} alt="Hand" />
-                          <p>{item.price}</p>
-                        </span>
-                      </button>
-                    </div>
-                    <div className="cta">
-                      <p>CTA</p>
-                      <button type="button">
-                        <span>
-                          <img src={Arrow} alt="Arrow" />
-                          <p>{item.price}</p>
-                        </span>
-                      </button>
+                    <div className="content-main">
+                      <div className="content-para-button">
+                        <div className="impression">
+                          <p>IMPRESSION</p>
+                          <button type="button">
+                            <span>
+                              <img src={Eye} alt="Eye" />
+                              <p>{item.Price}</p>
+                            </span>
+                          </button>
+                        </div>
+                        <div className="clicks">
+                          <p>CLICKS</p>
+                          <button type="button">
+                            <span>
+                              <img src={Hand} alt="Hand" />
+                              <p>{item.price}</p>
+                            </span>
+                          </button>
+                        </div>
+                        <div className="cta">
+                          <p>CTA</p>
+                          <button type="button">
+                            <span>
+                              <img src={Arrow} alt="Arrow" />
+                              <p>{item.price}</p>
+                            </span>
+                          </button>
+                        </div>
+                      </div>
+                      <div className="para">
+                        <p className="unitted">{item.Brand}</p>
+                        <p className="knitted">{item.Product_Name}</p>
+                      </div>
+                      <div className="button-div">
+                        <button>
+                          <Link to={`${item.Product_Uid}`}>
+                            VIEW PRODUCT DETAILS
+                          </Link>
+                        </button>
+                      </div>
                     </div>
                   </div>
-                  <div className="para">
-                    <p className="unitted">{item.Brand}</p>
-                    <p className="knitted">{item.Product_Name}</p>
-                  </div>
-                  <div className="button-div">
-                    <button>
-                      <Link to={`${item.Product_Uid}`}>
-                        VIEW PRODUCT DETAILS
-                      </Link>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </Col>
-          ))}
-      </Row>
+                </Col>
+              ))}
+          </Row>
+        </>
+      )}
     </div>
   );
 };
