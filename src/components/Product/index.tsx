da7fa12e3d2394a84929816095acc7f7e5089587
KrishnaPrabu-KL <krishna.prabu@kaaylabs.com>
2023-04-28 15:11:21 +0530

    empty data upated

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,6 +1,6 @@
 import React, { type FC, useEffect, useState } from 'react';
 import './content.scss';
-import { Col, Row, Spin } from 'antd';
+import { Col, Row, Spin, Empty } from 'antd';
 import { Link } from 'react-router-dom';
 import { get } from 'lodash';
 import { getProduct } from '../../shared/urlHelper';
@@ -38,68 +38,72 @@ const ProductList: FC = () => {
     <div className="head">
       {loading && (
         <div className="loader">
-          <Spin tip="Loading" size="large">
-            <div className="content" />
-          </Spin>
+          <Spin tip="Loading" size="large" />
         </div>
       )}
       {!loading && (
         <>
-          <Row className="content-row">
-            {todos &&
-              todos.map((item: any, index: number) => (
-                <Col key={index} sm={24} md={12} lg={8}>
-                  <div className="content-head">
-                    <div className="content-logo">
-                      <img
-                        src={item.Image === null ? DefaultImage : item.Image}
-                        alt={item.Brand}
-                      />
+          {todos.length > 0 ? (
+            <Row className="content-row">
+              {todos &&
+                todos.map((item: any, index: number) => (
+                  <Col key={index} sm={24} md={12} lg={8}>
+                    <div className="content-head">
+                      <div className="content-logo">
+                        <img
+                          src={item.Image === null ? DefaultImage : item.Image}
+                          alt={item.Brand}
+                        />
+                      </div>
+                      <div className="content-main">
+                        <div className="content-para-button">
+                          <div className="impression">
+                            <p>IMPRESSION</p>
+                            <button type="button">
+                              <span>
+                                <img src={Eye} alt="Eye" />
+                                <p>0</p>
+                              </span>
+                            </button>
+                          </div>
+                          <div className="clicks">
+                            <p>CLICKS</p>
+                            <button type="button">
+                              <span>
+                                <img src={Hand} alt="Hand" />
+                                <p>0</p>
+                              </span>
+                            </button>
+                          </div>
+                          <div className="cta">
+                            <p>CTA</p>
+                            <button type="button">
+                              <span>
+                                <img src={Arrow} alt="Arrow" />
+                                <p>0</p>
+                              </span>
+                            </button>
+                          </div>
+                        </div>
+                        <div className="para">
+                          <p className="unitted">{item.Brand}</p>
+                          <p className="knitted">{item.Product_Name}</p>
+                        </div>
+                        <div className="button-div">
+                          <Link to={`${item.Product_Uid}`}>
+                            <button>VIEW PRODUCT DETAILS</button>
+                          </Link>
+                        </div>
+                      </div>
                     </div>
-                    <div className="content-main">
-                      <div className="content-para-button">
-                        <div className="impression">
-                          <p>IMPRESSION</p>
-                          <button type="button">
-                            <span>
-                              <img src={Eye} alt="Eye" />
-                              <p>0</p>
-                            </span>
-                          </button>
-                        </div>
-                        <div className="clicks">
-                          <p>CLICKS</p>
-                          <button type="button">
-                            <span>
-                              <img src={Hand} alt="Hand" />
-                              <p>0</p>
-                            </span>
-                          </button>
-                        </div>
-                        <div className="cta">
-                          <p>CTA</p>
-                          <button type="button">
-                            <span>
-                              <img src={Arrow} alt="Arrow" />
-                              <p>0</p>
-                            </span>
-                          </button>
-                        </div>
-                      </div>
-                      <div className="para">
-                        <p className="unitted">{item.Brand}</p>
-                        <p className="knitted">{item.Product_Name}</p>
-                      </div>
-                      <div className="button-div">
-                        <Link to={`${item.Product_Uid}`}>
-                          <button>VIEW PRODUCT DETAILS</button>
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                </Col>
-              ))}
-          </Row>
+                  </Col>
+                ))}
+            </Row>
+          ) : (
+            <div className="empty">
+              <Empty />
+            </div>
+          )}
         </>
       )}
     </div>
